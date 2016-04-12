403c29927e183f3a2acbe498a149c0efa1455be2
Alina Böttcher <alina.boettcher@freenet.de>
2016-04-12 15:16:35 +0200

    Benutzereingabe hinzugefügt

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,5 +3,8 @@ import java.util.*;
 public class Studienberatung{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Hast du Abitur?");
+		String antwort = scanner.next();
 	}
 }
